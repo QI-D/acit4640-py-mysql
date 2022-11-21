d1de985c90994f7730fc5a280ed6ab94c6467e5b
QI-D <jonsondang@outlook.com>
2022-11-21 09:49:35 -0800

    update backend docker
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- backend/start.sh
+++ backend/start.sh
@@ -7,3 +7,13 @@ MYSQL_DB = ${MYSQL_DATABASE}
 MYSQL_USER = ${MYSQL_USER}
 MYSQL_PASSWORD = ${MYSQL_PASSWORD}
 " > backend.conf
+
+wait-for-it -h ${MYSQL_HOST} -p 3306 -t 0 -- hostname
+
+/app/.local/bin/gunicorn wsgi:app -b 0.0.0.0:8080 &
+
+wait-for-it -h localhost -p 8080 -t 0 -- hostname
+
+curl -X POST ${BACKEND_URL}/add -H "Content-Type: application/json" -d '{"name": "Qi", "bcit_id": "A01207761"}'
+
+wait
